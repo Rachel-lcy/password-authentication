0b72f98fcabc2b62f1786a2f65ba6c5798a0192b
Rachel <hurui.lcy@gmail.com>
2025-06-04 12:30:16 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+
+// setup a HTTP server  using express app. Add get method for home('/) route
+
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.get('/', (req, res)=> {
+
+})
